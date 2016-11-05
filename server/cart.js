d51e3f16c77d7d45966e738c1c5631c7ceac5283
Sharon <sharonghae@gmail.com>
2016-11-05 00:25:47 -0400

    hook up button

--- server/cart.js
+++ server/cart.js
@@ -6,17 +6,35 @@ const cart = require('express').Router()
 
     //GET ALL PRODUCTS IN ORDER
     .get('/', (req, res, next) => {
-        console.log("IN THE CART ROUTE")
-        OrderProduct.findAll({
-            where: {
-                order_id: req.session.orderId
-            }
-        })
-        .then(function(allProducts){
-            console.log("IN THE CART ROUTE, allProducts",allProducts)
-            res.send(allProducts)
-        })
-        .catch(next)
+        if(!req.user) {
+            OrderProduct.findAll({
+                where: {
+                    order_id: req.session.orderId
+                }
+            })
+            .then(function(allProducts){
+                res.send(allProducts)
+            })
+            .catch(next)
+        } else {
+            Order.findOrCreate({
+                where: {status: 'pending', user_id: req.user.id}
+                }
+            )
+            .then(function(order) {
+                return OrderProduct.findAll({
+                    where: {
+                        order_id: order[0].id
+                    }
+                })
+            })
+            .then(function(allProducts){
+                res.send(allProducts)
+            })
+            .catch(next)
+        }
+        
+
     })  
 
     //GET ALL PAST ORDERS
@@ -36,14 +54,14 @@ const cart = require('express').Router()
     .post('/', (req, res, next) => {
         if(req.user) {
             Order.findOrCreate({
-             where: {status: pending, user_id: req.user.id}
+             where: {status: 'pending', user_id: req.user.id}
             })
             .then(function(foundOrder) {  //req,body is product
-            req.session.orderId = foundOrder[0].id
-            return foundOrder[0].addProduct(req.body, {name: req.body.name, price: req.body.price})
+            //req.session.orderId = foundOrder[0].id
+            return foundOrder[0].addProduct(req.body.id, {name: req.body.name, price: req.body.price})
             })
             .then(function(order) {
-                res.sendStatus(201)
+                res.status(201).send(order)
             })
         }
         else {
@@ -51,18 +69,19 @@ const cart = require('express').Router()
                 Order.create({})
                 .then(function(createdOrder) {
                     req.session.orderId = createdOrder.id
-                    return createdOrder.addProduct(req.body, {name: req.body.name, price: req.body.price})
+
+                    return createdOrder.addProduct(req.body.id, {name: req.body.name, price: req.body.price})
                 })
                 .then(function(order) {
-                    res.sendStatus(201)
+                    res.status(201).send(order)
                 })
             } else {
                 Order.findById(req.session.orderId)
                 .then(function(foundOrder) {
-                    return foundOrder.addProduct(req.body, {name: req.body.name, price: req.body.price})
+                    return foundOrder.addProduct(req.body.id, {name: req.body.name, price: req.body.price})
                 })
                 .then(function(order) {
-                    res.sendStatus(201)
+                    res.status(201).send(order)
                 })
             }
         }
