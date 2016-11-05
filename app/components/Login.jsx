2ff910d12ecf899993551bd2d4bc10a4970c462f
Sharon <sharonghae@gmail.com>
2016-11-05 12:55:36 -0400

    rerender current cart when logout/login

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router';
 
-export const Login = ({ login }) => (
-  <li className='dropdown'>
+export class Login extends Component{
+  componentDidMount() {
+    this.props.fetchOrderDetails()
+  }
+  render() {
+    return (
+        <li className='dropdown'>
 
-    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login/Sign Up<span className="caret"></span></a>
+        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login/Sign Up<span className="caret"></span></a>
 
-    <ul className='dropdown-menu'>
-      <li>
-        <form onSubmit={evt => {
-        evt.preventDefault()
-        login(evt.target.username.value, evt.target.password.value)
-        }}>
-        <input name="username" />
-        <input name="password" type="password" />
-        <input type="submit" value="Login" />
-      </form>
+        <ul className='dropdown-menu'>
+          <li>
+            <form onSubmit={evt => {
+              evt.preventDefault()
+              this.props.login(evt.target.username.value, evt.target.password.value)
+            }}>
+            <input name="username" />
+            <input name="password" type="password" />
+            <input type="submit" value="Login" />
+          </form>
+          </li>
+          <li role="separator" className="divider"></li>
+          <li>
+            <p>New user? Create an account</p>
+            <Link to="/signup" activeClassName="active">Sign Up</Link>
+          </li>
+          
+        </ul>
+
       </li>
-      <li role="separator" className="divider"></li>
-      <li>
-        <p>New user? Create an account</p>
-        <Link to="/signup" activeClassName="active">Sign Up</Link>
-      </li>
-      
-    </ul>
+    )
+  }
+} 
 
-  </li>
- )
 
-import {login} from 'APP/app/reducers/auth'
+
+import {login} from 'APP/app/reducers/auth';
+import { fetchOrderDetails } from 'APP/app/action-creators/cart'
 import {connect} from 'react-redux'
 
 
 export default connect (
   state => ({}),
-  {login},
+  {login, fetchOrderDetails },
 ) (Login)
--- app/components/WhoAmI.jsx
+++ app/components/WhoAmI.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export const WhoAmI = ({ user, logout }) => (
+export class WhoAmI extends Component {
+	componentDidMount() {
+		this.props.fetchOrderDetails()
+	}
 
-  <li className="whoami">
-  <a href="#">
-    Signed in as {user && user.name} 	&nbsp;
-  	<button className="btn btn-default navbar-btn logout-button" onClick={logout}>Logout</button>
-  </a>
-  </li>
+	render() {
+		return (
+			  <li className="whoami">
+			  <a href="#">
+			    Signed in as {this.props.user && this.props.user.name} 	&nbsp;
+			  	<button className="btn btn-default navbar-btn logout-button" onClick={this.props.logout}>Logout</button>
+			  </a>
+			  </li>
+		)
+	}
 
-)
+}
+
+
+
+
+
 
 import {logout} from 'APP/app/reducers/auth'
+import { fetchOrderDetails } from 'APP/app/action-creators/cart'
 import {connect} from 'react-redux'
 
 export default connect (
   ({ auth }) => ({ user: auth }),
-  {logout},
+  {logout, fetchOrderDetails},
 ) (WhoAmI)
